e2eb538a2f75af76539b446dab3f57602da19e72
Vlad-Lucky <vladsid0601@gmail.com>
2022-04-16 20:42:06 +0300

    Main file edited

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import threading
-from source_code.site.site_session import run
+from source_code.site import site_session
 from source_code.bots.discord import discord_session
 
 
 # запуск flask-сайта
-thread = threading.Thread(target=run)
+thread = threading.Thread(target=site_session.run)
 thread.start()
 # запуск дискорд бота
 discord_session.run()
